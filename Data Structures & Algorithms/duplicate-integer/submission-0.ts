54bf90b3d6332d9d9910cdacc3626096a2f2939f
Anan Saleh <238986810+Anan-Saleh-1995@users.noreply.github.com>
2026-06-27 21:15:50 +0300

    Suggest using Set for duplicate checking
    Added a comment suggesting the use of a Set for better performance.

--- Data Structures & Algorithms/duplicate-integer/submission-0.ts
+++ Data Structures & Algorithms/duplicate-integer/submission-0.ts
@@ -1,5 +1,6 @@
 class Solution {
     hasDuplicate(nums: number[]): boolean {
+        // can be done better with set since a key is not important here
         const seen = new Map<number, number>();
         for (let i = 0; i < nums.length; i++) {
             if(seen.has(nums[i])) {
