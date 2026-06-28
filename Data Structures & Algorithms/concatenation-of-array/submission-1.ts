af738ecc8b72623dd6b8a81d6bd3a9c09c4f2c53
Anan Saleh <238986810+Anan-Saleh-1995@users.noreply.github.com>
2026-06-28 20:44:36 +0300

    Enhance getConcatenation method with comments
    Added comments to explain the concatenation logic.

--- Data Structures & Algorithms/concatenation-of-array/submission-1.ts
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.ts
@@ -1,5 +1,26 @@
 class Solution {
     getConcatenation(nums: number[]): number[] {
+        // this about it like this
+        /**
+            ans[i] = nums[i];
+            ans[i + originalLength] = nums[i];
+
+            nums = [1, 2, 3, 4]
+            
+            ans[0] = num[0] -> 1
+            ans[4] = nums[0] -> 1
+
+            gradually it concatenate the array into two
+            ans[1] = nums[1] -> 2
+            ans[5] = nums[1] -> 2
+            
+            ans[2] = nums[2] -> 3
+            ans[6] = nums[2] -> 3
+
+            ans[3] = nums[3] -> 4
+            ans[8] = nums[3] -> 4
+
+        */
         const originalLength = nums.length;
         const expectedLengthResult = 2 * originalLength;
         let ans: number[] = new Array(expectedLengthResult).fill(0);
@@ -11,8 +32,6 @@ class Solution {
             }
             ans[i++] = nums[j++]
         }
-        console.log(ans)
         return ans;
-
     }
 }
