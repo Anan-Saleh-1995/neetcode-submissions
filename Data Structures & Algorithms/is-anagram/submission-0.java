4637200971e560f794654c6b8a9c12530d501d6e
Anan Saleh <238986810+Anan-Saleh-1995@users.noreply.github.com>
2026-07-08 18:49:52 +0300

    Enhance comments on space complexity for isAnagram
    Added comments regarding space complexity and potential optimizations for the isAnagram method.

--- Data Structures & Algorithms/is-anagram/submission-0.java
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -2,6 +2,14 @@
 Time Complexity: O(n)
 Space Complexity: O(n)
 */
+/*
+It is possible to achievie it with space complexity of O(1)
+But depends
+if String s and String t were known to be only from a-z then it's possible to use array of size of 26 allocated
+if upper case and lower case exists and allowed to do .toLowerCase() then do on both and avoid size of 128
+if any unicode characters then user solution below
+
+*/
 class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) {
