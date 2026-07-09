36dc79e22688c15147aa13b8464ce34721cf9b1a
Anan Saleh <238986810+Anan-Saleh-1995@users.noreply.github.com>
2026-07-09 07:31:08 +0300

    Add lessons i learned to replace-elements-with-greatest-element-on-right-side
    Explain how to make it O(N) and what i learned and add a note to remember Math library

--- Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-0.java
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-0.java
@@ -2,11 +2,23 @@ class Solution {
     /*
         space complexity O(1)
         time complexity O(N^2)
+
+        New word learned: Traversing
+        New way of thinking learned: try going from right to left or thinking about it
+
+        by going right to left i can perceive the maximum and remember it and then making it one run at the array
+
+        this results in 
+        space complexity of O(1)
+        time complexity of O(N)
+
+        note: remember to use Math library
+        
     */
     private int findMax(int start, int[] arr) {
         int max = arr[start];
         for (int i = start + 1; i < arr.length; i++) {
-            if (arr[i] > max) {
+            if (arr[i] > max) { /* I can use Math.max here */
                 max = arr[i];
             }
         }
