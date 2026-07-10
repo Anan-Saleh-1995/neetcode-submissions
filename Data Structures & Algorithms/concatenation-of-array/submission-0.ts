faf263168039ed2045b88e2e71d5e6509d4a3bf9
Anan Saleh <238986810+Anan-Saleh-1995@users.noreply.github.com>
2026-07-10 21:57:05 +0300

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.ts
@@ -0,0 +1,11 @@
+class Solution {
+    getConcatenation(nums: number[]): number[] {
+        const numsLength = nums.length;
+        const numsLengthTwice = numsLength * 2;
+        let ans: number[];
+
+        for (let i = 0; i < nums)
+
+        return ans;
+    }
+}
