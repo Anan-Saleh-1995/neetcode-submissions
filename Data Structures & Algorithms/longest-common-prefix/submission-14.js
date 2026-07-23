596bbd3dfa94df8de759c6f3a22f5d1e136ca6eb
Anan Saleh <238986810+Anan-Saleh-1995@users.noreply.github.com>
2026-07-23 15:04:14 +0300

    Add: longest-common-prefix - submission-14

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-14.js
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+        const map = {};
+        if (strs.length === 0) {
+            return "";
+        }
+        if (strs.length === 1) {
+            return strs[0];
+        }
+        for (let i = 0; i < strs.length; i++) {
+            let word = strs[i][0];
+            if (map[word]) {
+                map[word]++;
+            } else {
+                map[word] = 1;
+            }
+            for(let j = 1; j < strs[i].length; j++) {
+                word = word + strs[i][j];
+                if (map[word]) {
+                map[word]++;
+                } else {
+                map[word] = 1;
+                }
+            }
+        }
+        const keys = Object.keys(map);
+        let longestCommonPrefix = "";
+        for (let i = 0; i < keys.length; i++) {
+            if (keys[i].length === 0) {
+                continue;
+            }
+            if(keys[i].length > longestCommonPrefix.length) {
+                if(map[keys[i]] === strs.length) { // chatgpt helped me here after i gave up
+                    longestCommonPrefix = keys[i];
+                }
+            }
+        }
+        return longestCommonPrefix;
+    }
+}
