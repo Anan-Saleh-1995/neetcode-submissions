d7f2e7e096bf6b8449704ffb4062862fe5da4413
Anan Saleh <238986810+Anan-Saleh-1995@users.noreply.github.com>
2026-07-30 17:17:10 +0300

    Update submission-5.ts

--- Data Structures & Algorithms/is-subsequence/submission-5.ts
+++ Data Structures & Algorithms/is-subsequence/submission-5.ts
@@ -5,19 +5,6 @@ class Solution {
      * @return {boolean}
      */
 
-    /**
-     * 
-     *         int nextCharIndex = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (nextCharIndex == t.length()) {
-                break;
-            }
-            if (s.charAt(i) == t.charAt(nextCharIndex)) {
-                nextCharIndex++;
-            }
-        }
-     * 
-     */
     isSubsequence(s: string, t: string): boolean {
         let nextCharIndex: number = 0;
         for (let i: number = 0; i < t.length; i++) {
