2a100fd3c4eeb02c695456be4ca9c3a50ff61305
Anan Saleh <238986810+Anan-Saleh-1995@users.noreply.github.com>
2026-08-03 13:35:05 +0300

    Add: island-perimeter - submission-2

---
+++ Data Structures & Algorithms/island-perimeter/submission-2.js
@@ -0,0 +1,88 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    islandPerimeter(grid) {
+        const conditions = (num) => ({
+            [`0${num}`]: { left: 1, top: 1 },
+            /**
+             * same thing but oppsite with the last row
+             * bottom is always 1
+             */
+        });
+        let perimeter = 0;
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid[i].length; j++) {
+                if (i === 0 && grid[i][j] === 1) {
+                    perimeter++;
+                    // automatically upper side is + 1
+                }
+                if (j === 0 && grid[i][j] === 1) {
+                    perimeter++;
+                    // automatically left side is + 1
+                }
+                if (j === grid[i].length - 1 && grid[i][j] === 1) {
+                    perimeter++;
+                    // automatically right side is + 1
+                }
+
+                // here we take care of neighbors
+                if (grid[i][j] === 1) {
+                    // if i is first dont check upper
+                    // and j is 0 don't left
+                    // if i is last don't check
+
+                    if (j !== grid[i].length - 1) {
+                        if (grid[i][j+1] === 0) {
+                            perimeter++;
+                        }
+                    }
+
+                    if (j !== 0) {
+                        if (grid[i][j-1] === 0) {
+                            perimeter++;
+                        }
+                    }
+
+                    if (j !== 0 && i !== 0) {
+                        if (grid[i - 1][j] === 0) {
+                            perimeter++;
+                        }
+                    }
+
+                    if (j === 0 && i !== 0) {
+                        if (grid[i - 1][j] === 0) {
+                            perimeter++;
+                        }
+                    }
+
+                    if (i !== grid.length - 1) {
+                        if (grid[i + 1][j] === 0) {
+                            perimeter++;
+                        }
+                    }
+
+
+
+
+
+                    // so im left with if im 1 and not all up there
+                    // i check my nieghbors 
+                    // that means if im 1 and my right is 0
+                    // i count ++
+                    // if im 1 and my down is 0 i count ++
+                    // if im 1 and my left is 0 i count ++
+                    // if im 1 and my up is 0 i count ++
+                }
+
+                if (i === grid.length - 1 && grid[i][j] === 1) {
+                    perimeter++;
+                    // automatically down side is + 1
+                }
+
+            }
+        }
+        return perimeter;
+    }
+}
