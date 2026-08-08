ac6c8d91cff1d18887a9b740c2d667171e6b4286
Anan Saleh <238986810+Anan-Saleh-1995@users.noreply.github.com>
2026-08-08 16:12:17 +0300

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums, val) {
+        let right = nums.length - 1;
+        let left = 0;
+        let k = 0;
+        while (right > left) {
+            if (nums[left] === val) {
+                if (nums[right] !== val) {
+                    nums[left++] = nums[right]
+                }
+                right--;
+            } else {
+                left++;
+            }
+            k++;
+        }
+        return k;
+    }
+}
