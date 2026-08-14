0f4a552080217af138d307d8ed7a5871e0a0dc3b
Anan Saleh <238986810+Anan-Saleh-1995@users.noreply.github.com>
2026-08-14 11:04:52 +0300

    Add: unique-email-addresses - submission-2

---
+++ Data Structures & Algorithms/unique-email-addresses/submission-2.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string[]} emails
+     * @return {number}
+     */
+    numUniqueEmails(emails) {
+        let count = 0;
+        const set = new Set();
+        for(let i = 0; i < emails.length; i++) {
+            const [localName, domainName] = emails[i].split('@');
+            const domainNameSplit = domainName.split('.');
+            const localNameSplit = localName.split('+');
+            const localNameSplitSplitted = localNameSplit[0].split('.');
+            let newString;
+            for (let j = 0; j<localNameSplitSplitted.length;j++) {
+                newString = newString ? newString+localNameSplitSplitted[j] : localNameSplitSplitted[j];
+            }
+            let newString2;
+            for (let k = 0; k<domainNameSplit.length;k++) {
+                newString2 = newString2 ? newString2+domainNameSplit[k] : domainNameSplit[k];
+            }
+            console.log(newString2);
+            console.log(newString, domainName);
+            if (!set.has(newString) || !set.has(domainName)) {
+                if (!set.has(newString)) {
+
+                    set.add(newString);
+                } 
+                if (!set.has(domainName)) {
+                    set.add(domainName)
+                }
+                count++;
+            }
+        }
+        return count;
+    }
+}
