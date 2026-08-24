eac542d153184dfa06fb73514e4d15e5e0346ea3
Anan Saleh <238986810+Anan-Saleh-1995@users.noreply.github.com>
2026-08-24 06:46:03 +0300

    Add: design-hashset - submission-1

---
+++ Data Structures & Algorithms/design-hashset/submission-1.js
@@ -0,0 +1,41 @@
+class MyHashSet {
+    constructor(initial) {
+        this.init = initial;
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    add(key) {
+        if (!this.init) {
+            this.init = [key];
+        } else {
+            this.init.push(key);
+        }
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    remove(key) {
+        this.init = this.init.filter((i) => i !== key);
+    }
+
+    /**
+     * @param {number} key
+     * @return {boolean}
+     */
+    contains(key) {
+        return this.init?.includes(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
