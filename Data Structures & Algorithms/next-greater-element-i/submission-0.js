0c7087ed31ab5cc68f8d1baf73501cc06374d483
Anan Saleh <238986810+Anan-Saleh-1995@users.noreply.github.com>
2026-09-01 07:44:11 +0300

    Add: next-greater-element-i - submission-0

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-0.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number[]}
+     */
+    nextGreaterElement(nums1, nums2) {
+        // brute force way
+        const result = [];
+        for (let i = 0; i < nums1.length; i++) {
+            for (let j = 0; j < nums2.length; j++) {
+                if (nums1[i] === nums2[j]) {
+                    if (i === nums1.length - 1) {
+                        result.push(-1);
+                    } else {
+                        let counter = j + 1;
+                        while(counter < nums2.length) {
+                            if (nums2[counter] > nums1[i]) {
+                                result.push(nums2[counter]);
+                                break;
+                            }
+                                counter++;
+                        }
+                        if (counter === nums2.length) {
+                            result.push(-1);
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
