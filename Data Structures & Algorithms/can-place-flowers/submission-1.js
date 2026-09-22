d40d5dbcad361362d04dbbb6ac061d2b09cd5c43
Anan Saleh <238986810+Anan-Saleh-1995@users.noreply.github.com>
2026-09-22 17:19:13 +0300

    Add: can-place-flowers - submission-1

---
+++ Data Structures & Algorithms/can-place-flowers/submission-1.js
@@ -0,0 +1,48 @@
+class Solution {
+    /**
+     * @param {number[]} flowerbed
+     * @param {number} n
+     * @return {boolean}
+     */
+    canPlaceFlowers(flowerbed, n) {
+        let counter = 0;
+        let flagfront = 0;
+        let flagback = 0;
+        if (flowerbed[0] === 0 && flowerbed[1] === 0) {
+            counter++;
+            flagfront = 1;
+        }
+        if (flowerbed[flowerbed.length - 1] === 0 && flowerbed[flowerbed.length - 2] === 0) {
+            counter++;
+            flagback = 1;
+        }
+        let left = 0;
+        let right = flowerbed.length;
+        if (flagfront) {
+            left = 2;
+        }
+
+        if (flagback) {
+            right = flowerbed.length - 3;
+        }
+        while(left <= right) {
+            if (flowerbed[left] === 0) {
+                if (flowerbed[left + 1] === 0) {
+                    if (flowerbed[left + 2] === 0) {
+                        counter += 1;
+                        left += 2;
+                        continue;
+                    }
+                    left+=2;
+                    continue;
+                }
+            }
+            left++;
+        }
+        if (counter >= n) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
