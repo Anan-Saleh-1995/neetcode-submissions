955b04ba0f39e7acc32dee773d8b25bb405a8ac4
Anan Saleh <238986810+Anan-Saleh-1995@users.noreply.github.com>
2026-09-23 10:07:09 +0300

    Add: maximum-difference-between-even-and-odd-frequency-i - submission-0

---
+++ Data Structures & Algorithms/maximum-difference-between-even-and-odd-frequency-i/submission-0.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    maxDifference(s) {
+        const countMap = new Map();
+        for (let i = 0; i < s.length; i++) {
+            if (!countMap.has(s[i])) {
+                countMap.set(s[i], 1);
+            } else {
+                const currentCount = countMap.get(s[i]);
+                countMap.set(s[i], currentCount+1);
+            }
+        }
+        let maxOdd = 0;
+        let maxEven = 0;
+
+        countMap.forEach((value, key, map) => {
+            if ((value % 2) === 0) {
+                maxEven = Math.max(maxEven, value);
+            } else {
+                maxOdd = Math.max(maxOdd, value);
+            }
+        })
+
+        return maxOdd - maxEven;
+    }
+}
